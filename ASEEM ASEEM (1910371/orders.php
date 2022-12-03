979fefebe3014315a54ae37083c6105eefdc4aec
AseemGit <117101328+AseemGit@users.noreply.github.com>
2022-12-02 23:58:34 -0500

    ASEEM ASEEM (1910371)            1 Dec 2022    Created Project
    ASEEM ASEEM (1910371)            1 Dec 2022    Created Project - Three pages, index, orders, and buy design

---
+++ ASEEM ASEEM (1910371/orders.php
@@ -0,0 +1,167 @@
+<!-- 
+#DEVELOPER  DATE  COMMENTS
+#ASEEM ASEEM(1910371) 2022-01-30 Created PHP 
+ -->
+<html>
+<head>
+<title>education</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
+</head>
+<body>
+<div>
+  <header class="site-header">
+    <!-- Top header Close -->
+    <div class="main-header">
+      <div class="container">
+        <div class="logo-wrap" > <a href="index.php"><img src="images/logo.png" width="120" alt="Logo Image"></a>
+          <!-- <h1>Education</h1> -->
+        </div>
+        <div class="nav-wrap">
+          <nav class="nav-desktop">
+            <ul class="menu-list">
+              <li><a href="index.php">HOME</a></li>
+              <li> <a href="buying.php">BUY FOOD </a></li>
+              <li> <a href="orders.php">ORDERS</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container page-spec">
+    <div class="form-sec">
+      <h2 class="top-heading text-center">Orders</h2>
+      <!-- The form -->
+    </div>
+    <div class="clear"> </div>
+    <div class="table-css">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <thead style="background:#cccccc">
+          <tr>
+            <th align="left">Product<br> 
+            ID</th>
+            <th align="left">First <br>
+            name</th>
+            <th align="left">Last <br>
+            name</th>
+            <th align="left">City</th>
+            <th align="left">Comments</th>
+            <th align="right">Price</th>
+            <th align="right">Quantity </th>
+            <th align="right">Subtotal </th>
+            <th align="right">Taxes </th>
+            <th align="right">Grand total </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left">Phelmet4b</td>
+            <td align="left">Ben </td>
+            <td align="left">Masvidal </td>
+            <td align="left">Montr&eacute;al</td>
+            <td>&nbsp;</td>
+            <td align="right">49.99$</td>
+            <td align="right">1</td>
+            <td align="right">99.98$ </td>
+            <td align="right">16.10$</td>
+            <td align="right">116.08$ </td>
+          </tr>
+          <tr>
+            <td align="left">Pgloves675 </td>
+            <td align="left">Justin </td>
+            <td align="left">Legault</td>
+            <td align="left">Qu&eacute;bec</td>
+            <td align="left">10 % rebate</td>
+            <td align="right">22.49$</td>
+            <td align="right">2</td>
+            <td align="right">22.49$</td>
+            <td align="right">3.62$</td>
+            <td align="right">26.11$ </td>
+          </tr>
+          <tr>
+            <td align="left">Phelmet4b</td>
+            <td align="left">Ben </td>
+            <td align="left">Masvidal </td>
+            <td align="left">Montr&eacute;al</td>
+            <td>&nbsp;</td>
+            <td align="right">49.99$</td>
+            <td align="right">1</td>
+            <td align="right">99.98$ </td>
+            <td align="right">16.10$</td>
+            <td align="right">116.08$ </td>
+          </tr>
+          <tr>
+            <td align="left">Pgloves675 </td>
+            <td align="left">Justin </td>
+            <td align="left">Legault</td>
+            <td align="left">Qu&eacute;bec</td>
+            <td align="left">10 % rebate</td>
+            <td align="right">22.49$</td>
+            <td align="right">2</td>
+            <td align="right">22.49$</td>
+            <td align="right">3.62$</td>
+            <td align="right">26.11$ </td>
+          </tr>
+          <tr>
+            <td align="left">Phelmet4b</td>
+            <td align="left">Ben </td>
+            <td align="left">Masvidal </td>
+            <td align="left">Montr&eacute;al</td>
+            <td>&nbsp;</td>
+            <td align="right">49.99$</td>
+            <td align="right">1</td>
+            <td align="right">99.98$ </td>
+            <td align="right">16.10$</td>
+            <td align="right">116.08$ </td>
+          </tr>
+          <tr>
+            <td align="left">Pgloves675 </td>
+            <td align="left">Justin </td>
+            <td align="left">Legault</td>
+            <td align="left">Qu&eacute;bec</td>
+            <td align="left">10 % rebate</td>
+            <td align="right">22.49$</td>
+            <td align="right">2</td>
+            <td align="right">22.49$</td>
+            <td align="right">3.62$</td>
+            <td align="right">26.11$ </td>
+          </tr>
+          <tr>
+            <td align="left">Phelmet4b</td>
+            <td align="left">Ben </td>
+            <td align="left">Masvidal </td>
+            <td align="left">Montr&eacute;al</td>
+            <td>&nbsp;</td>
+            <td align="right">49.99$</td>
+            <td align="right">1</td>
+            <td align="right">99.98$ </td>
+            <td align="right">16.10$</td>
+            <td align="right">116.08$ </td>
+          </tr>
+          <tr>
+            <td align="left">Pgloves675 </td>
+            <td align="left">Justin </td>
+            <td align="left">Legault</td>
+            <td align="left">Qu&eacute;bec</td>
+            <td align="left">10 % rebate</td>
+            <td align="right">22.49$</td>
+            <td align="right">2</td>
+            <td align="right">22.49$</td>
+            <td align="right">3.62$</td>
+            <td align="right">26.11$ </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<footer class="page-footer">
+  <div class="footer-last-section">
+    <div class="container">
+      <p>&copy;  Copyright ASEEM ASEEM (1910371) 2022.</p>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
